a123248e80f90801bde1956878a415c60a3379d0
lsl001006 <1958698497@qq.com>
2022-10-10 20:44:37 +0800

    update

--- scripts/train_pix2pix_Nuclei.sh
+++ scripts/train_pix2pix_Nuclei.sh
@@ -1,6 +1,5 @@
 set -ex
 
-python train.py --dataroot /share_hd1/db/Nuclei --name brats_gan_nuclei_withoutL1 --model pix2pix --netG resnet_9blocks --direction AtoB --lambda_L1 0 --delta_perceptual 10 --dataset_mode nuclei --pool_size 0 --gpu_ids 2 --batch_size 12 --num_threads 0 --continue_train --niter=200 --niter_decay=200
+# python train.py --dataroot /data/repo/code/1sl/FullNet-varCE/original_data/MultiOrgan/AsynDGAN_Nuclei_dataset --name brats_gan_nuclei_withoutL1 --model pix2pix --netG resnet_9blocks --direction AtoB --lambda_L1 0 --delta_perceptual 10 --dataset_mode nuclei --pool_size 0 --gpu_ids 0 --batch_size 12 --num_threads 0  --niter=200 --niter_decay=200
 
-
-
+python save_syn_nuclei.py --dataroot /data/repo/code/1sl/FullNet-varCE/original_data/MultiOrgan/AsynDGAN_Nuclei_dataset --name brats_gan_nuclei_withoutL1 --model pix2pix --netG resnet_9blocks --direction AtoB  --dataset_mode nuclei  --gpu_ids 0 --batch_size 12 --num_threads 0  
--- train.sh
+++ train.sh
@@ -1,3 +1,3 @@
 python train.py --dataroot ./datasets/BRATS/splitedH5 \
---dataset_mode brats_split --gpu_ids 1 --batch_size 1 \
+--dataset_mode brats_split --gpu_ids 0 --batch_size 1 \
 --name bratsTest0929_test --model asyndgan --direction BtoA --num_netD 2
